c2f400977e732753fe7d308ccc00e3f530434e94
Djobkz <jonybkz@gmail.com>
2026-09-25 04:07:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-/*
-  BOZA FOODS CONFIGURATION
-  1. Create a Supabase project.
-  2. Copy your Project URL and anon public key here.
-  3. NEVER put a service_role key in this file.
-*/
+
 window.BOZA_CONFIG = {
   SUPABASE_URL: "Https://glhphqlahkljataogmwy.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_HP1ra_YVmZEQ42swzIp9tQ_nUuPTR2s"
